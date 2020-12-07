c5240ba2654ce5c38ac172b3f5f6acd54960119f
Polina <darmograyp@yandex.ru>
2020-12-07 18:53:07 +0200

    07122020

---
+++ 07122020/07122020num3.py
@@ -0,0 +1,11 @@
+#Реализовать функцию my_func(), которая принимает три позиционных аргумента, и возвращает
+# сумму наибольших двух аргументов.
+
+def my_func(a,b,c):
+    if a > c and b > c:
+        return a + b
+    elif a > b and a < c:
+        return a + c
+    else:
+        return b + c
+print(my_func(int(input('Введите первое число = ')),int(input('Введите второе число = ')),int(input('Введите третье число = '))))
---
+++ 07122020/07122020num6.py
@@ -0,0 +1,4 @@
+def int_func():
+    a = input('Введите строку, состоящую из букв нижнего регистра: ')
+    print(a.title())
+int_func()
