28300cf74150450dcc6aa3cfeb738f8013fdb82a
Полина Дармограй <71552405+Raz00movskaya@users.noreply.github.com>
2020-12-09 22:15:04 +0200

    Delete 07122020num2.py

--- 07122020/07122020num2.py
+++
@@ -1,4 +0,0 @@
-def info(username, userlastname, yearofbirth, city, email, phone):
-    print(f"Имя - {username}; Фамилия - {userlastname}; Год рождения - {yearofbirth}; Город проживания - {city}; Email - {email}; Телефон - {phone} ")
-#
-info(username=input('Введите имя: '), userlastname=input('Введите фамилию: '), yearofbirth=input('Введите год рождения: '), city=input('Введите город: '), email=input('Введите email: '), phone=input('Введите телефон: '))
